21d13c9025986bfb0ae97ceb14691667396fcd05
omar <omarchahidi@outlook.fr>
2020-09-06 23:04:33 +0200

    Commit n°44 06/09/2020 : - Ajout de l'entité Adresse - Ajout du paramètre tva dans l'entité Article - Calcul du prix HT, prix du tva et du prix TTC dans la parti validation commande - Ajout des contraints sur l'article, utlisateur et Article - Ne pas ajouter une variante dans le panier lorsqu'on arrive à la quantité du stock - Donner la possibilité d'ajouter des adresses d'un utilisateur connecté - Donner la possibilité de supprimer une adresse pour un utilisateur connecté - Valider adresse livraison et facturation - Préparer commande - Passer commande - Créer commande - Créer commande produit - Ajouter ligne facture dans DB - Ajouter ligne bon livraison dans DB - Modifier ligne variante (stocke) dans DB - Ajouter historique des commandes - Création facture

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -16,10 +16,40 @@ class FactureController extends AbstractController
         //$depotCommande = $this->getDoctrine()->getRepository(Commande::class);
         //$commande = $depotCommande->findOneBy(array('utilisateur' => $this->getUser(), 'id' => $commande->getId() ) );
 
+        if(!$commande){
+            // Type = info, success, warning ou danger
+            $this->addFlash('danger', 'Il y a un problème de téléchargement de la commande');
+            return $this->redirectToRoute("afficher_les_commandes", [
+                'id' => $this->getUser()->getId(),
+            ]);
+        }
         //dump($commande);
         //die();
         return $this->render('facture/creerFactureDeLaCommande.html.twig', [
             'commande' => $commande,
         ]);
     }
+
+    /**
+     * @Route("/facture/creerPDF/{id}", name="creer_facture_pdf")
+     */
+    public function creerFactureDeLaCommandeFormatPDF(Commande $commande)
+    {
+        //$depotCommande = $this->getDoctrine()->getRepository(Commande::class);
+        //$commande = $depotCommande->findOneBy(array('utilisateur' => $this->getUser(), 'id' => $commande->getId() ) );
+
+        if(!$commande){
+            // Type = info, success, warning ou danger
+            $this->addFlash('danger', 'Il y a un problème de téléchargement de la commande');
+            return $this->redirectToRoute("afficher_les_commandes", [
+                'id' => $this->getUser()->getId(),
+            ]);
+        }
+        //dump($commande);
+        //die();
+        return $this->render('facture/creerFactureDeLaCommande.html.twig', [
+            'commande' => $commande,
+        ]);
+    }
+
 }
